b532d7b253a937aef27c1399050cbc3c5d489167
btgibbons <bgibbons@bluefinrobotics.com>
2016-03-30 14:14:15 -0400

    Add working sysroot

---
+++ vehicle/adapt-sysroot/ros_install_isolated/lib/python2.7/dist-packages/topic_tools/srv/__init__.py
@@ -0,0 +1,4 @@
+from ._MuxAdd import *
+from ._MuxList import *
+from ._MuxDelete import *
+from ._MuxSelect import *
---
+++ vehicle/adapt-sysroot/ros_install_isolated/share/topic_tools/cmake/topic_tools-msg-extras.cmake
@@ -0,0 +1,2 @@
+set(topic_tools_MESSAGE_FILES "")
+set(topic_tools_SERVICE_FILES "srv/MuxAdd.srv;srv/MuxDelete.srv;srv/MuxList.srv;srv/MuxSelect.srv")
